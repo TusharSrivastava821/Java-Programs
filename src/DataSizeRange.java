a3bde262abc1fb977cbb86283f4d6a2a3b7ead54
Tushar Srivastava <tushar812srivastava@gmail.com>
2021-12-05 14:53:37 +0530

    Added new program

---
+++ src/DataSizeRange.java
@@ -0,0 +1,10 @@
+class DataSizeRange
+{
+public static void main(String arg[])
+{
+System.out.println("INT min:"+Integer.MIN_VALUE);
+System.out.println("INT max:"+Integer.MAX_VALUE);
+System.out.println("INT size:"+Integer.SIZE);
+System.out.println("INT byte:"+Integer.BYTES);
+}
+}
