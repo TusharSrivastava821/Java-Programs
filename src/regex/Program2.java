33ecf9738c09d491393520f2797bddf263da7567
Tushar Srivastava <tushar812srivastava@gmail.com>
2021-12-14 23:11:51 +0530

    Added new program

---
+++ src/regex/Program2.java
@@ -0,0 +1,26 @@
+// Write a regular expression to represent all names end with J | j .
+
+package regex;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Program2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("Enter string : ");
+        str = sc.nextLine();
+        Pattern p = Pattern.compile("\\b[A-Za-z]+[jJ]+\\b");
+        Matcher m =  p.matcher(str);
+        int ctr = 0;
+        while(m.find())
+        {
+            System.out.println(m.group());
+            ctr++;
+        }
+
+    }
+}
